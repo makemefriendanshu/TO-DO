1bcfef289e0093ee71659ea2ac9f9519833ed4bc
Anshuman <makemefriend.anshu@gmail.com>
2024-02-05 13:20:13 +0530

    add dev mailbox

--- lib/TODO_web/router.ex
+++ lib/TODO_web/router.ex
@@ -23,7 +23,7 @@ defmodule TODOWeb.Router do
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:todo, :dev_routes) do
+  # if Application.compile_env(:todo, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
@@ -37,7 +37,7 @@ defmodule TODOWeb.Router do
       live_dashboard "/dashboard", metrics: TODOWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
+  # end
 
   ## Authentication routes
 
