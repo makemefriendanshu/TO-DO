de718b86949f2cb053efb80367a90b614726ceef
Anshuman <makemefriend.anshu@gmail.com>
2024-09-20 08:59:47 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TODO.MixProject do
     [
       app: :todo,
       version: "0.1.0",
-      elixir: "~> 1.12.0",
+      elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
